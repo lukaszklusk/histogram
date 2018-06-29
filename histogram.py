ca13e82e393ee2345c55d0cbea2364a564ed939c
unknown <asd@zxc.com>
2018-06-29 18:46:27 +0200

    update 5

--- histogram.py
+++ histogram.py
@@ -1,31 +1,32 @@
-import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 from tkinter import *
+
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
 
+
 class Okienko(Frame):
-    def input_i_t(self,inpt):
+    def input_i_t(self, inpt):
         if not inpt:
             self.i = 0
             return
         self.i = int(inpt)
 
-    def input_n_t(self,inpt):
+    def input_n_t(self, inpt):
         if not inpt:
             self.n = 1000
             return
         self.n = int(inpt)
 
-    def input_u_t(self,inpt):
+    def input_u_t(self, inpt):
         if not inpt:
             self.u = 0
             return
         self.u = float(inpt)
 
-    def input_s_t(self,inpt):
+    def input_s_t(self, inpt):
         if not inpt:
             self.s = 1
             return
@@ -37,13 +38,13 @@ class Okienko(Frame):
         self.input_s_t(self.input_s.get())
         self.x = np.random.normal(self.u, self.s, self.n)
 
-    def rys_2(self,v):
-        f = Figure(figsize=(5,4),dpi=100)
+    def rys_2(self, v):
+        f = Figure(figsize=(5, 4), dpi=100)
         a = f.add_subplot(111)
-        a.hist(self.x,v)
+        a.hist(self.x, v)
         canvas = FigureCanvasTkAgg(f, self)
         canvas.show()
-        canvas.get_tk_widget().grid(row=9)
+        canvas.get_tk_widget().grid(row=9,columnspan=2)
 
     def rys_hist(self):
         d = int(round(np.sqrt(self.n)))
@@ -55,26 +56,26 @@ class Okienko(Frame):
 
     def createWidgets(self):
         self.label1 = Label(self)
-        self.label1["text"] = "Ilosc przedzialow(dla 0 jest rowna pierwiaskowi z ilosci danych)"
+        self.label1["text"] = "Ilość przediałów(domyślna wartość to pierwiastek z ilości danych)"
         self.label1.grid(row=1)
 
         self.label2 = Label(self)
-        self.label2["text"] = "Ilosc danych(dane sa generowane na podstawie rozkladu normalnego"
+        self.label2["text"] = "Ilość danych(dane są generowane na podstawie rozkładu normalnego)"
         self.label2.grid(row=4)
 
         self.label3 = Label(self)
-        self.label3["text"] = "Wartosc oczekiwana"
+        self.label3["text"] = "Wartość oczekiwana"
         self.label3.grid(row=5)
 
         self.label4 = Label(self)
         self.label4["text"] = "Odchylenie standardowe"
         self.label4.grid(row=6)
 
-        self.input_i = Entry(self,bd=5)
+        self.input_i = Entry(self, bd=5)
         self.input_i["width"] = 6
-        self.input_i.grid(row=1,column=1)
-        self.input_i.delete(0,END)
-        self.input_i.insert(0,"0")
+        self.input_i.grid(row=1, column=1)
+        self.input_i.delete(0, END)
+        self.input_i.insert(0, "0")
 
         self.input_n = Entry(self, bd=5)
         self.input_n["width"] = 6
@@ -97,15 +98,15 @@ class Okienko(Frame):
         self.rysuj = Button(self)
         self.rysuj["text"] = "Rysuj histogram"
         self.rysuj["command"] = self.rys_hist
-        self.rysuj.grid(row=2,column=1,rowspan=2)
+        self.rysuj.grid(row=2, column=1, rowspan=2)
 
         self.gen = Button(self)
         self.gen["text"] = "Generuj nowe dane"
         self.gen["command"] = self.gen_dane
-        self.gen.grid(row=7,column=1,rowspan=2)
+        self.gen.grid(row=7, column=1, rowspan=2)
 
-    def __init__(self,master=None):
-        Frame.__init__(self,master)
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
         self.x = np.random.normal(0, 1, 1000)
         self.n = 1000
         self.i = 0
@@ -115,8 +116,9 @@ class Okienko(Frame):
         self.createWidgets()
         self.rys_2(int(round(np.sqrt(self.n))))
 
+
 root = Tk()
 root.title("Histogram")
-root.geometry("800x600")
+root.geometry("570x570")
 app = Okienko(master=root)
 root.mainloop()
